81021e62cfb928d27450b2cced11501d7bdcbace
Пользователь <zditovetskaya.d@yandex.ru>
2018-04-28 08:52:40 +0300

    modified app

--- app.js
+++ app.js
@@ -1,64 +1,73 @@
+"use strict";
 function PressEnter(your_text, your_event) {
     if(your_text != "" && your_event.keyCode == 13)
         search();
 }
 
-function search() {
-    "use strict";
-    fetch(`https://api.github.com/users/${document.getElementById('uname').value}`)
-        .then(response =>
-        {
+function getInfo(uName) {
+    const url = 'https://api.github.com/users/' + uName;
+    return request(url);
+}
+
+function request(url) {
+    return fetch(url)
+        .then(response =>{
             if (document.getElementById('err')) {
                 document.getElementById('content').removeChild(document.getElementById('err'));
             }
             if (response.status >= 200 && response.status < 400)
                 return response.json();
-            else {
+            else{
                 const err = document.createElement('h1');
                 err.innerText=`User with login ${document.getElementById('uname').value} does not exist`;
                 document.getElementById('content').appendChild(err);
                 err.id = 'err';
-             }
-        })
-        .then((user) => {
-            document.getElementById('uname').value = "";
-            const cont = document.getElementById('content');
-            if (document.getElementById('info')) {
-                cont.removeChild(document.getElementById('info'));
             }
-            const container = document.createElement('div');
-            cont.appendChild(container);
-            container.id = 'info';
-            let elements = [document.createElement('img'), document.createElement('h1'),
-                document.createElement('h2'), document.createElement('h3'), document.createElement('h3'),
-                document.createElement('h4'), document.createElement('a'), document.createElement('a')];
-            elements[0].src = user["avatar_url"];
-            elements[1].innerText = user["name"];
-            elements[2].innerText = user["login"];
-            elements[3].innerText = user["bio"];
-            elements[4].innerText = user["company"];
-            elements[5].innerText = user["location"];
-            elements[6].innerText = user["email"];
-            elements[7].innerText = user["blog"];
-            //console.log(container.childNodes);
-            for(let m = 1; m < elements.length; m++){
-                if(elements[m].innerText === ""){
-                    elements.splice(m, 1);
-                    m--;
-                }
-            }
-           if (container.childNodes) {
-               elements.map((item) => {
-                   container.appendChild(item);
-               });
-           }
-            else{
-               while (container.firstChild)
-                   container.removeChild(container.firstChild);
-               elements.map((item) => {
-                   container.appendChild(item);
-               });
-           }
         })
+}
+
+function createDiv(user) {
+    document.getElementById('uname').value = "";
+    const cont = document.getElementById('content');
+    if (document.getElementById('info'))
+        cont.removeChild(document.getElementById('info'));
+    const container = document.createElement('div');
+    cont.appendChild(container);
+    container.id = 'info';
+    let elements = [document.createElement('img'), document.createElement('h1'),
+        document.createElement('h2'), document.createElement('h3'), document.createElement('h3'),
+        document.createElement('h4'), document.createElement('a'), document.createElement('a')];
+    elements[0].src = user["avatar_url"];
+    elements[1].innerText = user["name"];
+    elements[2].innerText = user["login"];
+    elements[3].innerText = user["bio"];
+    elements[4].innerText = user["company"];
+    elements[5].innerText = user["location"];
+    elements[6].innerText = user["email"];
+    elements[7].innerText = user["blog"];
+    for(let m = 1; m < elements.length; m++){
+        if(elements[m].innerText === ""){
+            elements.splice(m, 1);
+            m--;
+        }
+    }
+    if (container.childNodes) {
+        elements.map((item) => {
+            container.appendChild(item);
+        });
+    }
+    else{
+        while (container.firstChild)
+            container.removeChild(container.firstChild);
+        elements.map((item) => {
+            container.appendChild(item);
+        });
+    }
+}
+function search() {
+    getInfo(document.getElementById('uname').value)
+        .then((user) =>
+            createDiv(user)
+        )
         .catch((response) => console.log(`Error with status: ${response.status}`));
 }
