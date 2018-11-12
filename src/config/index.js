118cbb54a66551322bba080d269573a2d34d5426
Пользователь <zditovetskaya.d@yandex.ru>
2018-11-12 14:08:18 +0300

    wrap by try - catch JSON.parse

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,19 @@
 const constants = require('./constants');
 const fs = require('fs');
 const path = require('path');
-const file = fs.readFileSync(path.join(__dirname + constants.FILE_PATH));
 
-module.exports = JSON.parse(file);
+let temp;
+
+const parseFile = function(filePath) {
+    try {
+        const file = fs.readFileSync(filePath);
+        temp = JSON.parse(file);
+    } catch (e) {
+        console.error(e);
+        return 'File error';
+    }
+};
+
+parseFile(path.join(__dirname + constants.FILE_PATH));
+
+module.exports = temp;
